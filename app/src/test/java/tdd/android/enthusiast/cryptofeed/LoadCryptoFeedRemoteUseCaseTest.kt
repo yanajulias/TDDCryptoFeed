c8f3ca64ef066ca34e252e74193a1bd9b2c9009e
Yana <44026935+yanajulias@users.noreply.github.com>
2023-10-28 05:07:47 +0700

    `LoadCryptoFeedRemoteUseCase` doesn't request data upon creation

---
+++ app/src/test/java/tdd/android/enthusiast/cryptofeed/LoadCryptoFeedRemoteUseCaseTest.kt
@@ -0,0 +1,22 @@
+package tdd.android.enthusiast.cryptofeed
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class LoadCryptoFeedRemoteUseCase() {
+
+}
+
+class HttpClient() {
+    var getCount = 0
+}
+
+class LoadCryptoFeedRemoteUseCaseTest() {
+    @Test
+    fun testInitDoesNotLoad() {
+        val client = HttpClient()
+        LoadCryptoFeedRemoteUseCase()
+
+        assertTrue(client.getCount == 0)
+    }
+}
