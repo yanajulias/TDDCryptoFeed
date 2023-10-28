a3d36a54425e39bd863212b65d74865fbb495a2a
Yana <44026935+yanajulias@users.noreply.github.com>
2023-10-28 19:35:36 +0700

    Request data once on load

--- app/src/test/java/tdd/android/enthusiast/cryptofeed/LoadCryptoFeedRemoteUseCaseTest.kt
+++ app/src/test/java/tdd/android/enthusiast/cryptofeed/LoadCryptoFeedRemoteUseCaseTest.kt
@@ -1,22 +1,42 @@
 package tdd.android.enthusiast.cryptofeed
 
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class LoadCryptoFeedRemoteUseCase() {
-
+    fun load() {
+        HttpClient.instance.getCount = 1
+    }
 }
 
-class HttpClient() {
+class HttpClient private constructor() {
+    companion object {
+        var instance = HttpClient()
+    }
+
     var getCount = 0
 }
 
 class LoadCryptoFeedRemoteUseCaseTest() {
     @Test
     fun testInitDoesNotLoad() {
-        val client = HttpClient()
+        val client = HttpClient.instance
         LoadCryptoFeedRemoteUseCase()
 
-        assertTrue(client.getCount == 0)
+        assertTrue(client.getCount == 0) // memastikan kalau tidak di load sama sekali
+    }
+
+    @Test
+    fun testLoadRequestData() {
+        //Given
+        val client = HttpClient.instance
+        val sut = LoadCryptoFeedRemoteUseCase()
+
+        // When (action)
+        sut.load()
+
+        // Then
+        assertEquals(1, client.getCount) // cek kalo betul di load 1 kali
     }
 }
