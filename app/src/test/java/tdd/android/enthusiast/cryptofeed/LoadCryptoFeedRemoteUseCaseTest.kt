214b6066c1f5f9c009e681750531f270f91b5783
Yana <44026935+yanajulias@users.noreply.github.com>
2023-10-29 09:51:54 +0700

    Guarantee we only receive one error

--- app/src/test/java/tdd/android/enthusiast/cryptofeed/LoadCryptoFeedRemoteUseCaseTest.kt
+++ app/src/test/java/tdd/android/enthusiast/cryptofeed/LoadCryptoFeedRemoteUseCaseTest.kt
@@ -1,8 +1,12 @@
 package tdd.android.enthusiast.cryptofeed
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
+import tdd.android.enthusiast.cryptofeed.api.Connectivity
 import tdd.android.enthusiast.cryptofeed.api.HttpClient
 import tdd.android.enthusiast.cryptofeed.api.LoadCryptoFeedRemoteUseCase
 
@@ -39,6 +43,20 @@ class LoadCryptoFeedRemoteUseCaseTest() {
         assertEquals(2, client.getCount) // cek kalo betul di load 1 kali
     }
 
+    @Test
+    fun testLoadDeliversErrorOnClientError() = runBlocking {
+        val (sut, client) = makeSut()
+
+        client.error = Exception("Test")
+
+        val capturedError = arrayListOf<Exception>()
+        sut.load().collect { error ->
+            capturedError.add(error)
+        }
+
+        assertEquals(listOf(Connectivity::class.java), capturedError.map { it::class.java })
+    }
+
     private fun makeSut(): Pair<LoadCryptoFeedRemoteUseCase, HttpClientSpy> {
         val client = HttpClientSpy()
         val sut = LoadCryptoFeedRemoteUseCase(client = client)
@@ -47,8 +65,12 @@ class LoadCryptoFeedRemoteUseCaseTest() {
 
     private class HttpClientSpy : HttpClient {
         var getCount = 0
+        var error: Exception? = null
 
-        override fun get() {
+        override fun get(): Flow<Exception> = flow {
+            if (error != null) {
+                emit(error ?: Exception())
+            }
             getCount += 1
         }
     }
