58b8f595a95f07a665470ed1789338abd2cbc611
Yana <44026935+yanajulias@users.noreply.github.com>
2023-11-11 14:57:51 +0700

    Move remote models to it's own file

--- app/src/main/java/tdd/android/enthusiast/cryptofeed/api/LoadCryptoFeedRemoteUseCase.kt
+++ app/src/main/java/tdd/android/enthusiast/cryptofeed/api/LoadCryptoFeedRemoteUseCase.kt
@@ -1,47 +1,11 @@
 package tdd.android.enthusiast.cryptofeed.api
 
-import com.squareup.moshi.Json
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import tdd.android.enthusiast.cryptofeed.domain.CoinInfo
 import tdd.android.enthusiast.cryptofeed.domain.CryptoFeed
 import tdd.android.enthusiast.cryptofeed.domain.Raw
 import tdd.android.enthusiast.cryptofeed.domain.Usd
-
-data class RemoteRootCryptoFeed(
-    @Json(name = "Data")
-    val data: List<RemoteCryptoFeedItem>
-)
-
-data class RemoteCryptoFeedItem(
-    @Json(name = "CoinInfo")
-    val remoteCoinInfo: RemoteCoinInfo,
-    @Json(name = "RAW")
-    val remoteRaw: RemoteDisplay
-)
-
-data class RemoteCoinInfo(
-    @Json(name = "id")
-    val id: String,
-    @Json(name = "Name")
-    val name: String,
-    @Json(name = "FullName")
-    val fullName: String,
-    @Json(name = "ImageUrl")
-    val imageUrl: String
-)
-
-data class RemoteDisplay(
-    @Json(name = "USD")
-    val usd: RemoteUsd
-)
-
-data class RemoteUsd(
-    @Json(name = "PRICE")
-    val price: Double,
-    @Json(name = "CHANGEPCTDAY")
-    val changePctDay: Float
-)
 
 sealed class HttpClientResult {
     data class Failure(val exception: Exception) : HttpClientResult()
