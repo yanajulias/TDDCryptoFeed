b86cdc105d0ac615d942f60bf353935a4e3698d4
Yana <44026935+yanajulias@users.noreply.github.com>
2023-11-26 04:33:25 +0700

    Delivers failure with `ConnectivityException` on request error

---
+++ app/src/test/java/tdd/android/enthusiast/cryptofeed/CryptoFeedRetrofitHttpClientTest.kt
@@ -0,0 +1,58 @@
+package tdd.android.enthusiast.cryptofeed
+
+import app.cash.turbine.test
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import tdd.android.enthusiast.cryptofeed.api.ConnectivityException
+import tdd.android.enthusiast.cryptofeed.api.HttpClientResult
+import java.io.IOException
+
+interface CryptoFeedService {
+    suspend fun get()
+}
+
+class CryptoFeedRetrofitHttpClient(
+    private val service: CryptoFeedService
+) {
+    fun get(): Flow<HttpClientResult> = flow{
+        try {
+            service.get()
+        } catch (exception: Exception){
+            emit(HttpClientResult.Failure(ConnectivityException()))
+        }
+    }
+}
+
+class CryptoFeedRetrofitHttpClientTest {
+    private val service = mockk<CryptoFeedService>()
+    private lateinit var sut: CryptoFeedRetrofitHttpClient
+
+    @Before
+    fun setUp() {
+        sut = CryptoFeedRetrofitHttpClient(service = service)
+    }
+
+    @Test
+    fun testGetFailsOnConnectivityError() = runBlocking {
+        coEvery { // karena pake suspend function
+            service.get()
+        } throws IOException()
+
+        sut.get().test {
+            val receivedValue = awaitItem() as HttpClientResult.Failure
+            assertEquals(ConnectivityException()::class.java, receivedValue.exception::class.java)
+            awaitComplete()
+        }
+
+        coVerify {
+            service.get()
+        }
+    }
+}
