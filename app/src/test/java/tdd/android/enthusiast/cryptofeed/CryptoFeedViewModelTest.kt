11fe9393cdb62d7a99e0de0d553f300756710f28
Yana <44026935+yanajulias@users.noreply.github.com>
2023-12-02 09:51:30 +0700

    `CryptoFeedViewModel` does not load crypto feed upon creation

--- app/src/test/java/tdd/android/enthusiast/cryptofeed/CryptoFeedViewModelTest.kt
+++ app/src/test/java/tdd/android/enthusiast/cryptofeed/CryptoFeedViewModelTest.kt
@@ -1,14 +1,20 @@
 package tdd.android.enthusiast.cryptofeed
 
 import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.confirmVerified
+import io.mockk.spyk
+import io.mockk.verify
 import junit.framework.TestCase.assertFalse
 import junit.framework.TestCase.assertTrue
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.flowOf
 import org.junit.Before
 import org.junit.Test
 import tdd.android.enthusiast.cryptofeed.domain.CryptoFeed
+import tdd.android.enthusiast.cryptofeed.domain.LoadCryptoFeedUseCase
 
 data class UiState(
     val isLoading: Boolean = false,
@@ -16,26 +22,38 @@ data class UiState(
     val failed: String = ""
 )
 
-class CryptoFeedViewModel{
+class CryptoFeedViewModel(private val useCase: LoadCryptoFeedUseCase) {
     private val _uiState = MutableStateFlow(UiState())
     val uiState: StateFlow<UiState> = _uiState.asStateFlow()
+
+    fun load(){}
 }
 
 class CryptoFeedViewModelTest {
-    private lateinit var sut : CryptoFeedViewModel
+    private val useCase = spyk<LoadCryptoFeedUseCase>()
+    private lateinit var sut: CryptoFeedViewModel
 
     @Before
-    fun setUp(){
+    fun setUp() {
         MockKAnnotations.init(this, relaxed = true)
-        sut = CryptoFeedViewModel()
+        sut = CryptoFeedViewModel(useCase = useCase)
     }
 
     @Test
-    fun testInitInitialState(){
+    fun testInitInitialState() {
         val uiState = sut.uiState.value
         assertFalse(uiState.isLoading)
         assertTrue(uiState.cryptoFeed.isEmpty())
         assert(uiState.failed.isEmpty())
 
+    }
+
+    @Test
+    fun testInitDoesNotLoad() {
+        verify(exactly = 0) {
+            useCase.load()
+        }
+
+        confirmVerified(useCase)
     }
 }
